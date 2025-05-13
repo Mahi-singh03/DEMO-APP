57e83a9754e4afb51afa8a406b5c70db122e1f63
Mahi singh <aka.mahisingh@gmail.com>
2025-05-13 16:45:49 +0530

    new added

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,16 @@
 export default function Home() {
   return (
-    <div>HELLO</div>
-  )
+    <div className="fixed inset-0 z-[-1]">
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="w-full h-full object-cover"
+      >
+        <source src="/straegy.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
 }
